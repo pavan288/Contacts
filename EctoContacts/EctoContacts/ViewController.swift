613636d7e32407dafc914953ee48883b4ec8ba30
Pavan Powani <pavan.powani@practo.com>
2020-05-09 19:01:30 +0530

    Creates EctoContacts

---
+++ EctoContacts/EctoContacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EctoContacts
+//
+//  Created by Pavan Powani on 09/05/20.
+//  Copyright © 2020 PavanPowani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
