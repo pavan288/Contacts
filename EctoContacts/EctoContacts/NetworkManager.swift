683322e233ecb32bcd902dc913e187d74184986d
Pavan Powani <pavan.powani@practo.com>
2020-05-10 14:25:22 +0530

    Add support for POST calls in Network Manager

--- EctoContacts/EctoContacts/NetworkManager.swift
+++ EctoContacts/EctoContacts/NetworkManager.swift
@@ -14,10 +14,26 @@ class NetworkManager {
 
     private init() {}
 
-    func get(urlString: String, parameters: [String: Any]? = nil, completion: @escaping (Data?, URLResponse?, Error?) -> Void) {
-        guard let url = URL(string: urlString) else { return }
+    func getParamsString(params: [String: String]) -> String {
+        var keyValueArray = [String]()
+        for key in params.keys {
+            let paramsString = key + "=" + (params[key]!)
+            keyValueArray.append(paramsString)
+        }
+        return keyValueArray.joined(separator: "&")
+    }
+
+    func get(urlString: String, parameters: [String: String]? = nil, completion: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        var requestUrl = urlString
+        if let httpParams = parameters {
+            let paramsString: String = getParamsString(params: httpParams)
+            requestUrl = urlString + "?" + paramsString
+        }
+        guard let url = URL(string: requestUrl) else { return }
         let session = URLSession.shared
-        let request = URLRequest(url: url)
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+
         let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
             guard let data = data else { return }
             DispatchQueue.main.async {
@@ -30,4 +46,26 @@ class NetworkManager {
         })
         task.resume()
     }
+
+    func post(urlString: String, parameters: [String: String]? = nil, completion: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        guard let url = URL(string: urlString) else { return }
+        let session = URLSession.shared
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("Application/json", forHTTPHeaderField: "Content-Type")
+        let httpBody = try? JSONSerialization.data(withJSONObject: parameters as Any, options: [])
+        request.httpBody = httpBody
+        let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
+            guard let data = data else { return }
+            DispatchQueue.main.async {
+                if error != nil {
+                    completion(nil, response, error)
+                } else {
+                    completion(data, response, nil)
+                }
+            }
+        })
+        task.resume()
+    }
+
 }
